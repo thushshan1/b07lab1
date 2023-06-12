d21826252979b7ccf2fc5b21fd183f7d55321278
Thushshan Rameswaran <thushshan123@gmail.com>
2023-06-11 22:58:30 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,36 @@
-public class Driver{
-	public static void main(String [] args){
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1)=" + s.evaluate(0.1));
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
+import java.io.File;
+public class Driver
+{
+    public static void main(String [] args)
+    {
+        Polynomial p = new Polynomial();
+        System.out.println(p.evaluate(3));
+        double [] c1 = {4,6};
+        int [] d1 = {0,1};
+        Polynomial p1 = new Polynomial(c1, d1);
+        double [] c2 = {3,2};
+        int [] d2 = {0, 1};
+        Polynomial p2 = new Polynomial(c2, d2);
+        Polynomial s = p1.add(p2);
+        System.out.println("s(0.1) = " + s.evaluate(0.1));
+        if(s.hasRoot(1))
+            System.out.println("1 is a root of s");
+        else
+            System.out.println("1 is not a root of s");
+        Polynomial multi = p1.multiply(p2);
+        for(int i = 0; i < multi.array.length; i++)
+        {
+            System.out.println(multi.array[i] + ", Exponent: " + multi.exponents[i]);
+        }
+        System.out.println("multi(2) = " + multi.evaluate(2));
+		System.out.println("Using file");
+		File input1 = new File("C:\\Users\\thush\\Desktop\\CSCB07\\b07lab1\\poly.txt");
+		Polynomial fileuser = new Polynomial(input1);
+		for(int i = 0; i < fileuser.array.length; i++)
+        {
+            System.out.println(fileuser.array[i] + ", Exponent: " + fileuser.exponents[i]);
+        }
+		p2.saveToFile("C:\\Users\\thush\\Desktop\\CSCB07\\b07lab1\\test.txt");
 		
-	}
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,36 +1,122 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+
 public class Polynomial{
 	
 	double array[] = new double[1];
+	int exponents[] = new int[1];
 	
 	public Polynomial(){
-		array[0] = 0;		
+		array[0] = 0;	
+		exponents[0] = 0;
 	}
 	
-	public Polynomial(double array1[])
+	public Polynomial(double array1[], int exp[])
 	{
 		this.array = new double[array1.length];
+		this.exponents = new int[exp.length];
 		
 		for(int y = 0; y <= (array1.length - 1); y++)
 		{
 			this.array[y] = array1[y];
 		}		
 		
+		for(int z = 0; z <= (exp.length - 1); z++)
+		{
+			this.exponents[z] = exp[z];
+		}
+	}
+	
+	public Polynomial(File poly)
+	{
+		BufferedReader input = null;
+		try
+		{
+			input = new BufferedReader(new FileReader(poly));
+			String num = input.readLine();
+			String num1[] = num.split("(?=[+-])");
+			double array2[] = new double[num1.length];
+			int exp[] = new int[num1.length];
+			int z = 0;
+			for(String num2 : num1)
+			{
+				String parts[] = num2.split("x");
+				array2[z] = Double.parseDouble(parts[0]);
+				if(parts.length > 1)
+				{
+					exp[z] = Integer.parseInt(parts[1]);
+				}
+				z++;
+			}
+			this.array = new double[array2.length];
+			this.exponents = new int[exp.length];
+			
+			for(int y = 0; y <= (array2.length - 1); y++)
+			{
+				this.array[y] = array2[y];
+			}
+			for(int t = 0; t <= (exp.length - 1); t++)
+			{
+				this.exponents[t] = exp[t];
+			}
+			input.close();
+			
+			
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+
+			
 	}
 	
 	public Polynomial add(Polynomial arr)
 	{
 		Polynomial check = new Polynomial();
-		if(this.array.length >= arr.array.length){
-			check = new Polynomial(this.array);
+		if(this.array.length >= arr.array.length)
+		{
+			check = new Polynomial(this.array, this.exponents);
 		}
-		else if(this.array.length < arr.array.length){
-			check = new Polynomial(arr.array);
+		else if(this.array.length < arr.array.length)
+		{
+			check = new Polynomial(arr.array, arr.exponents);
 		}
 		
-		for(int z = 0; z < (check.array.length - 1); z++)
+		int z = 0; 
+		int y = 0;
+		int q = 0;
+		while(z < this.array.length && y < arr.array.length)
 		{
-			check.array[z] = this.array[z] + arr.array[z];
-		}
+			if(this.exponents[z] == arr.exponents[y])
+			{
+				check.array[q] = this.array[z] + arr.array[y];
+				check.exponents[q] = this.exponents[z];
+				q++;
+				z++;
+				y++;
+			}
+			
+			else if(this.exponents[z] < arr.exponents[y])
+			{
+				check.array[q] = this.array[z];
+				check.exponents[q] = this.exponents[z];
+				q++;
+				z++;
+			}
+			else
+			{
+				check.array[q] = arr.array[y];
+				check.exponents[q] = arr.exponents[y];
+				q++;
+				y++;
+			}
+				
+		}	
 		
 		return check;
 
@@ -38,10 +124,10 @@ public class Polynomial{
 	
 	public double evaluate(double x)
 	{
-		double r = this.array[0];
-		for(int c = 1; c <= (this.array.length - 1); c++)
+		double r = 0;
+		for(int c = 0; c < this.exponents.length; c++)
 		{
-			r = r + this.array[c] * Math.pow(x, c);
+			r = r + this.array[c] * Math.pow(x, this.exponents[c]);
 		}
 		
 		return r;
@@ -56,7 +142,140 @@ public class Polynomial{
 		else{
 			return false;
 		}
-	}	
+	}
+	public boolean isin(int value)
+	{
+		boolean checker = false;
+		for(int q = 0; q < this.exponents.length; q++)
+		{
+			if(this.exponents[q] == value && value != 0)
+			{
+				checker = true;
+				break;
+			}
+		}
+		return checker;
+	}
+	
+	public Polynomial clean()
+	{
+		int x = 0;
+		int theLength = 0;
+		while(this.array[x] != 0)
+		{
+			theLength++;
+			x++;
+		}
+		double temp[] = new double[theLength];
+		int temp1[] = new int[theLength];
+		
+		Polynomial cleaned = new Polynomial(temp, temp1);
+		
+		for(int d = 0; d < theLength; d++)
+		{
+			cleaned.array[d] = this.array[d];
+			cleaned.exponents[d] = this.exponents[d];
+		}
+		
+		return cleaned;
+	}
+			
+		
+		
+
+	public Polynomial multiply(Polynomial pro)
+	{
+		double temp[] = new double[this.array.length * pro.array.length];
+		int temp1[] = new int[this.exponents.length * pro.exponents.length];
+		Polynomial check = new Polynomial(temp, temp1);
+		
+		int x = 0;
+		
+		while(x < check.array.length)
+		{
+			for(int y = 0; y < this.array.length; y++)
+			{
+				for(int z = 0; z < pro.array.length; z++)
+				{
+					check.array[x] = this.array[y] * pro.array[z];
+					check.exponents[x] = this.exponents[y] + pro.exponents[z];
+					x++;
+				}
+			}
+		}
+		Polynomial check1 = new Polynomial(temp, temp1);
+		int r = 0;
+		int y = 0;
+		int c = 0;
+		
+		while(r < check1.exponents.length)
+		{
+			
+			while(c < check.exponents.length)
+			{
+				while(y <  check.exponents.length)	
+				{
+					boolean checker1 = check1.isin(check.exponents[c]);
+
+					if(check.exponents[c] == check.exponents[y] && checker1 == false && c != y)
+					{
+						check1.array[r] = check.array[c] + check.array[y];
+						check1.exponents[r] = check.exponents[c];
+						r++;
+						break;
+					}
+					y++;
+				}
+				boolean checker1 = check1.isin(check.exponents[c]);
+				if(checker1 == false)
+				{
+					check1.array[r] = check.array[c];
+					check1.exponents[r] = check.exponents[c];
+					r++;
+				}
+				y = 0;
+				c++;
+				if(c == check.exponents.length)
+				{
+					r++;
+				}
+			}
+			
+		}
+		check1 = check1.clean();
+					
+		
+		return check1;
+		
+		
+	}
+	
+	public void saveToFile(String line)
+	{
+		
+		FileWriter output;
+		try{
+			output = new FileWriter(line);
+
+			for(int i = 0; i < array.length; i++)
+			{
+				if(i > 0 && array[i] > 0)
+					output.write("+");
+				output.write((int)array[i] + "");
+				if(i > 0) {
+					output.write("x");
+					output.write(exponents[i] + "");
+				}
+				}
+			output.write("\n");
+			output.close();
+			
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		
+	}
+			
 	
 	
 }
